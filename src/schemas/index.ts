7d507974b144d5bb4b481e08b6d3a59647d5e27e
rodrigopeakone <rodrigo@peakonedev.com>
2026-05-04 23:26:04 -0300

    feat: add get workout plan endpoint

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -43,6 +43,22 @@ export const HomeDataSchema = z.object({
   ),
 });
 
+export const GetWorkoutPlanSchema = z.object({
+  id: z.uuid(),
+  name: z.string(),
+  workoutDays: z.array(
+    z.object({
+      id: z.uuid(),
+      weekDay: z.enum(WeekDay),
+      name: z.string(),
+      isRest: z.boolean(),
+      coverImageUrl: z.url().optional(),
+      estimatedDurationInSeconds: z.number(),
+      exercisesCount: z.number(),
+    }),
+  ),
+});
+
 export const WorkoutPlanSchema = z.object({
   id: z.uuid(),
   name: z.string().trim().min(1),
